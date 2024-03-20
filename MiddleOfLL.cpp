26248c8b6cf67752dd04ccf940a87e7b1d19d942
John Zhou <83607969+johnzhou1210@users.noreply.github.com>
2024-03-20 17:29:51 -0400

    Create MiddleOfLL.cpp

---
+++ MiddleOfLL.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        // determine size first
+        int size = 0;
+        ListNode* curr_node = head;
+        while (curr_node != nullptr) {
+            size++;
+            curr_node = curr_node->next;
+        }
+        int stop_point = (size / 2) + 1;
+        for (int i = 0; i < stop_point - 1; ++i) {
+            head = head->next;
+        }
+        return head;
+    }
+};
