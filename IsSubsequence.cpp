79e3ea4f85b552d56b0297a320ff47fdca188fd3
John Zhou <83607969+johnzhou1210@users.noreply.github.com>
2024-05-14 15:42:05 -0400

    Create IsSubsequence.cpp

---
+++ IsSubsequence.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int ptr1 = 0, ptr2 = 0;
+        while (ptr2 < t.length()) {
+            if (s[ptr1] == t[ptr2]) {
+                ptr1++;
+            }
+            ptr2++;
+        }
+        return ptr1 == s.length();
+    }
+};
