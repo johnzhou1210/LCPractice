05a846e10f5fe61332d9f904cb1b803b85b57d06
John Zhou <chowjohn10@gmail.com>
2025-06-29 14:57:21 -0400

    Create fashion-trends.py

---
+++ tip102/unit-4/fashion-trends.py
@@ -0,0 +1,33 @@
+def find_trending_materials(brands):
+    frequencies = dict()
+    for brand in brands:
+        for material in brand['materials']:
+            frequencies[material] = 1 if not material in frequencies else frequencies[material] + 1
+    result = []
+    for k,v in frequencies.items():
+        if v > 1:
+            result.append(k)
+    return result
+
+
+brands = [
+    {"name": "EcoWear", "materials": ["organic cotton", "recycled polyester"]},
+    {"name": "GreenThreads", "materials": ["organic cotton", "bamboo"]},
+    {"name": "SustainableStyle", "materials": ["bamboo", "recycled polyester"]}
+]
+
+brands_2 = [
+    {"name": "NatureWear", "materials": ["hemp", "linen"]},
+    {"name": "Earthly", "materials": ["organic cotton", "hemp"]},
+    {"name": "GreenFit", "materials": ["linen", "recycled wool"]}
+]
+
+brands_3 = [
+    {"name": "OrganicThreads", "materials": ["organic cotton"]},
+    {"name": "EcoFashion", "materials": ["recycled polyester", "hemp"]},
+    {"name": "GreenLife", "materials": ["recycled polyester", "bamboo"]}
+]
+
+print(find_trending_materials(brands))
+print(find_trending_materials(brands_2))
+print(find_trending_materials(brands_3))
